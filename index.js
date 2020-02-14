6b5126806434b27775fe2c58b1568c79be03b432
danzjamz <djl.hovley@gmail.com>
2020-02-14 08:25:49 -0700

    updated index

---
+++ index.js
@@ -0,0 +1,23 @@
+// # Homework 02-13
+// ### Git and Github
+// - Create a new folder called git-homework-part-two
+// - Create a file inside that folder called index.js
+// - Initialize git
+// - Push to github
+
+// - Go back to the terminal
+// - Create a new branch called branchOne
+// - On line one of your index.js file add a console.log(‘Hi’)
+// - Push this branch up to github
+// - Merge that branch into your master branch on github
+// - Go back to the terminal
+// - Switch back to the master branch
+// - Pull down the latest code
+// - Create a new branch called branchTwo
+// - On line two of your index.js file declare a variable called name
+// - On line three assign that variable a value of your name
+// - Inside the terminal
+// - Switch back to the master branch
+// - Merge this branch into your master branch
+
+console.log('Hi');
