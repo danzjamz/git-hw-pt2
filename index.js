377b04cd3bac3baf004a7c25fb9986daf5b4cf3f
danzjamz <djl.hovley@gmail.com>
2020-02-14 08:38:46 -0700

    added to index

--- index.js
+++ index.js
@@ -4,7 +4,6 @@
 // - Create a file inside that folder called index.js
 // - Initialize git
 // - Push to github
-
 // - Go back to the terminal
 // - Create a new branch called branchOne
 // - On line one of your index.js file add a console.log(‘Hi’)
@@ -16,8 +15,11 @@
 // - Create a new branch called branchTwo
 // - On line two of your index.js file declare a variable called name
 // - On line three assign that variable a value of your name
+
 // - Inside the terminal
 // - Switch back to the master branch
 // - Merge this branch into your master branch
 
 console.log('Hi');
+const name;
+name = "Danielle";
