b54d08af4c01fdf317bfbdc12a1a3962c7dc086a
danzjamz <djl.hovley@gmail.com>
2020-02-17 17:25:24 -0700

    retrieved from stash

--- index.js
+++ index.js
@@ -1,25 +1,43 @@
-// # Homework 02-13
+// # Homework 02-17
 // ### Git and Github
-// - Create a new folder called git-homework-part-two
-// - Create a file inside that folder called index.js
-// - Initialize git
-// - Push to github
-// - Go back to the terminal
-// - Create a new branch called branchOne
-// - On line one of your index.js file add a console.log(‘Hi’)
-// - Push this branch up to github
-// - Merge that branch into your master branch on github
-// - Go back to the terminal
-// - Switch back to the master branch
-// - Pull down the latest code
-// - Create a new branch called branchTwo
-// - On line two of your index.js file declare a variable called name
-// - On line three assign that variable a value of your name
-// - Inside the terminal
-// - Switch back to the master branch
-// - Merge this branch into your master branch
+// - Open your old homework folder git-homework-part-two
+// - Delete branchOne locally
+// - If branchOne isn’t on github please push it up
+// - Delete branchOne on GitHub
+// - Create a new branch called stash-practice
+// - Add a console log to your index.js
+// - Stash your changes and swap back over to the master branch
+// - Add a new file called new-file.py
+// - Add a print(‘hello’)
+// - Add, commit and push that up to github
+// - Swap back to your stash-practice branch
+// - Bring back your stashed changes
+
+// - Add, commit and push that up to github
+// - Don’t forget to clear your stash
+// - Merge the stash-practice to your master branch on GitHub
+// - Swap back to the master branch on your terminal and either use pull or fetch
+// - Revert the index.js file back to the initial commit
+// - DON’T Push this last change to Github
+// ### Helpful Git Commands
+// ```
+// $ git init
+// $ git status
+// $ git add .
+// $ git commit -m “your commit msg”
+// $ git push
+// $ git branch
+// $ git checkout -b branchName
+// $ git checkout branchName
+// $ git merge branchName
+// $ git branch -d branchName
+// $ git log
+// $ git checkout commitId fileName.extension
+// ```
 
 
 console.log('Hi');
 const name;
 name = "Danielle";
+
+console.log('hei hei')
